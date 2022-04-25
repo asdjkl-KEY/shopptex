a1a34ff167b95aeec0acccb435c8c638e0bfb1dc
Jesse <asdjkl.key@gmail.com>
2022-04-25 11:42:12 -0300

    Navbar definitivo

--- js/main.js
+++ js/main.js
@@ -23,10 +23,32 @@ topBar.addEventListener('click', (e) => {
     e.preventDefault();
     let nav = document.getElementById('navbar');
     if(!topBar.ispress) {
-        nav.style.animation = 'navDown 0.8s forwards';
+        nav.style.animation = 'navDown 1s forwards';
         return topBar.ispress = true;
     } else {
-        nav.style.animation = 'navUp 0.8s forwards';
+        nav.style.animation = 'navUp 3s forwards';
         return topBar.ispress = false;
     }
 });
+
+
+function mainAnimation () {
+    let veicle = '';
+    let motorcycle = document.getElementById('motorcycle');
+    let truck = document.getElementById('truck');
+    let plane = document.getElementById('plane');
+    motorcycle.style.animation = 'veicleMove cubic-bezier(0,0,1,1) 8s'
+    motorcycle.addEventListener('animationend', () => {
+        motorcycle.style.animation = 'none';
+        truck.style.animation = 'veicleMove cubic-bezier(0,0,1,1) 8s';
+    });
+    truck.addEventListener('animationend', () => {
+        truck.style.animation = 'none';
+        plane.style.animation = 'planeMove cubic-bezier( 0, 0, 1, 1 ) 8s';
+    });
+    plane.addEventListener('animationend', () => {
+        plane.style.animation = 'none';
+        motorcycle.style.animation = 'veicleMove cubic-bezier(0,0,1,1) 8s'
+    });
+}
+mainAnimation();
